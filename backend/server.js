e6c93e1569b32bff0d8dfdca6ea9ed1a54626f9b
robin-dc <robindelacruz725@gmail.com>
2024-04-09 15:20:17 +0800

    added userRouter

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 const connect = require('./config/connection')
 const morgan = require('morgan');
 const cors = require('cors');
-// const errorHandler = require('./middleware/errorMiddleware')
+const errorHandler = require('./middleware/errorMiddleware')
+
 // MIDDLEWARE
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
@@ -13,14 +14,18 @@ app.use(morgan("dev"))
 
 
 // ROUTES
-// const authRouter = require('./routes/authRoute')
-// const postRouter = require('./routes/postRoute')
+const authRouter = require('./routes/authRoute')
+const postRouter = require('./routes/postRoute')
+const userRouter = require('./routes/userRoute')
 
-// app.get('/api/auth', authRouter)
-// app.get('/api/posts', postRouter)
+app.get('/api/auth', authRouter)
+
+// you cant access this if youre not authenticated
+app.get('/api/posts', postRouter)
+app.get('/api/users', userRouter)
 
 // ERROR MIDDLEWARE
-// app.use(errorHandler)
+app.use(errorHandler)
 
 const port = process.env.PORT
 
